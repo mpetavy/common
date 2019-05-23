a96fda12f376c52cd11d319d09a4cda34c4d250a
mpetavy <mpetavy>
2019-05-23 10:41:58 +0200

    ScanLinesWithLF

--- io.go
+++ io.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"os"
@@ -390,4 +391,20 @@ func CleanPath(path string) string {
 	path = filepath.Clean(path)
 
 	return path
+}
+
+func ScanLinesWithLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, data[0:i + 1], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
