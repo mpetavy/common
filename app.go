a538346c40127e7d96d21b60e2e90b17bafea1f2
mpetavy <mpetavy>
2019-07-11 08:26:24 +0200

    show banner on error

--- app.go
+++ app.go
@@ -118,6 +118,7 @@ func New(application *App, mandatoryFlags []string) {
 			fl := flag.Lookup(f)
 
 			if fl == nil {
+				ShowBanner()
 				Error(fmt.Errorf("unknown mandatory flag: %s", f))
 
 				flagErr = true
@@ -126,6 +127,7 @@ func New(application *App, mandatoryFlags []string) {
 			}
 
 			if len(fl.Value.String()) == 0 {
+				ShowBanner()
 				Error(fmt.Errorf("mandatory flag needed: \"-%s\" - %s", fl.Name, fl.Usage))
 
 				flagErr = true
