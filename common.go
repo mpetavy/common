5034c85f69023d3302d240dad456a5fec4d027c5
petavy@gmx.net <petavy@gmx.net>
2019-09-13 13:21:51 +0200

    discover --> common

--- common.go
+++ common.go
@@ -19,6 +19,39 @@ func init() {
 	shutdownHooks = make([]func(), 0)
 }
 
+type ChannelError struct {
+	m sync.Mutex
+	l []error
+}
+
+func (c *ChannelError) Add(err error) {
+	c.m.Lock()
+	c.l = append(c.l, err)
+	c.m.Unlock()
+}
+
+func (c *ChannelError) Get() error {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	if len(c.l) > 0 {
+		return c.l[0]
+	} else {
+		return nil
+	}
+}
+
+func (c *ChannelError) GetAll() []error {
+	return c.l
+}
+
+func (c *ChannelError) Exists() bool {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	return len(c.l) > 0
+}
+
 // Exit exist app and run all registered shutdown hooks
 func Done() {
 	onceDone.Do(func() {
