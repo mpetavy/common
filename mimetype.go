55cc263f4f24cc937d7be87d70a1fecf30c4c305
Marcel Petavy <marcel.petavy@zeiss.com>
2019-10-17 15:42:41 +0200

    variable renaming

--- mimetype.go
+++ mimetype.go
@@ -9,175 +9,175 @@ import (
 )
 
 const (
-	MIMETYPE_HEADER_LEN = 1024
+	MimetypeHeaderLen = 1024
 )
 
-type MIMETYPE_EXTENSION struct {
+type MimetypeExtension struct {
 	MimeType, Ext string
 }
 
-var MIMETYPES []MIMETYPE_EXTENSION
+var Mimetypes []MimetypeExtension
 
 var (
-	MIMETYPE_APPLICATION_DICOM,
-	MIMETYPE_APPLICATION_EPUB_ZIP,
-	MIMETYPE_APPLICATION_GEO_JSON,
-	MIMETYPE_APPLICATION_GML_XML,
-	MIMETYPE_APPLICATION_GPX_XML,
-	MIMETYPE_APPLICATION_GZIP,
-	MIMETYPE_APPLICATION_JAR,
-	MIMETYPE_APPLICATION_JAVASCRIPT,
-	MIMETYPE_APPLICATION_JSON,
-	MIMETYPE_APPLICATION_MSWORD,
-	MIMETYPE_APPLICATION_OCTET_STREAM,
-	MIMETYPE_APPLICATION_OGG,
-	MIMETYPE_APPLICATION_PDF,
-	MIMETYPE_APPLICATION_POSTSCRIPT,
-	MIMETYPE_APPLICATION_VND_GARMIN_TCX_XML,
-	MIMETYPE_APPLICATION_VND_GOOGLE_EARTH_KML_XML,
-	MIMETYPE_APPLICATION_VND_MS_EXCEL,
-	MIMETYPE_APPLICATION_VND_MS_POWERPOINT,
-	MIMETYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_PRESENTATION,
-	MIMETYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_SHEET,
-	MIMETYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT,
-	MIMETYPE_APPLICATION_WASM,
-	MIMETYPE_APPLICATION_X_7Z_COMPRESSED,
-	MIMETYPE_APPLICATION_X_CHROME_EXTENSION,
-	MIMETYPE_APPLICATION_X_JAVA_APPLET,
-	MIMETYPE_APPLICATION_X_PHOTOSHOP,
-	MIMETYPE_APPLICATION_X_PYTHON,
-	MIMETYPE_APPLICATION_X_SHOCKWAVE_FLASH,
-	MIMETYPE_APPLICATION_X_TAR,
-	MIMETYPE_APPLICATION_ZIP,
-	MIMETYPE_AUDIO_AIFF,
-	MIMETYPE_AUDIO_AMR,
-	MIMETYPE_AUDIO_APE,
-	MIMETYPE_AUDIO_BASIC,
-	MIMETYPE_AUDIO_FLAC,
-	MIMETYPE_AUDIO_MIDI,
-	MIMETYPE_AUDIO_MP4,
-	MIMETYPE_AUDIO_MPEG,
-	MIMETYPE_AUDIO_MUSEPACK,
-	MIMETYPE_AUDIO_WAV,
-	MIMETYPE_AUDIO_X_M4A,
-	MIMETYPE_FONT_WOFF,
-	MIMETYPE_FONT_WOFF2,
-	MIMETYPE_IMAGE_BMP,
-	MIMETYPE_IMAGE_GIF,
-	MIMETYPE_IMAGE_JPEG,
-	MIMETYPE_IMAGE_PNG,
-	MIMETYPE_IMAGE_SVG_XML,
-	MIMETYPE_IMAGE_TIFF,
-	MIMETYPE_IMAGE_WEBP,
-	MIMETYPE_IMAGE_X_ICON,
-	MIMETYPE_MODEL_VND_COLLADA_XML,
-	MIMETYPE_MODEL_X3D_XML,
-	MIMETYPE_TEXT_HTML,
-	MIMETYPE_TEXT_CSS,
-	MIMETYPE_TEXT_PLAIN,
-	MIMETYPE_TEXT_RTF,
-	MIMETYPE_TEXT_XML,
-	MIMETYPE_TEXT_X_LUA,
-	MIMETYPE_TEXT_X_PERL,
-	MIMETYPE_TEXT_X_PHP,
-	MIMETYPE_TEXT_X_TCL,
-	MIMETYPE_VIDEO_3GPP,
-	MIMETYPE_VIDEO_3GPP2,
-	MIMETYPE_VIDEO_MP4,
-	MIMETYPE_VIDEO_MPEG,
-	MIMETYPE_VIDEO_QUICKTIME,
-	MIMETYPE_VIDEO_WEBM,
-	MIMETYPE_VIDEO_X_FLV,
-	MIMETYPE_VIDEO_X_MATROSKA,
-	MIMETYPE_VIDEO_X_MSVIDEO MIMETYPE_EXTENSION
+	MimetypeApplicationDicom,
+	MimetypeApplicationEpubZip,
+	MimetypeApplicationGeoJson,
+	MimetypeApplicationGmlXml,
+	MimetypeApplicationGpxXml,
+	MimetypeApplicationGzip,
+	MimetypeApplicationJar,
+	MimetypeApplicationJavascript,
+	MimetypeApplicationJson,
+	MimetypeApplicationMsword,
+	MimetypeApplicationOctetStream,
+	MimetypeApplicationOgg,
+	MimetypeApplicationPdf,
+	MimetypeApplicationPostscript,
+	MimetypeApplicationVndGarminTcxXml,
+	MimetypeApplicationVndGoogleEarthKmlXml,
+	MimetypeApplicationVndMsExcel,
+	MimetypeApplicationVndMsPowerpoint,
+	MimetypeApplicationVndOpenxmlformatsOfficedocumentPresentationmlPresentation,
+	MimetypeApplicationVndOpenxmlformatsOfficedocumentSpreadsheetmlSheet,
+	MimetypeApplicationVndOpenxmlformatsOfficedocumentWordprocessingmlDocument,
+	MimetypeApplicationWasm,
+	MimetypeApplicationX7ZCompressed,
+	MimetypeApplicationXChromeExtension,
+	MimetypeApplicationXJavaApplet,
+	MimetypeApplicationXPhotoshop,
+	MimetypeApplicationXPython,
+	MimetypeApplicationXShockwaveFlash,
+	MimetypeApplicationXTar,
+	MimetypeApplicationZip,
+	MimetypeAudioAiff,
+	MimetypeAudioAmr,
+	MimetypeAudioApe,
+	MimetypeAudioBasic,
+	MimetypeAudioFlac,
+	MimetypeAudioMidi,
+	MimetypeAudioMp4,
+	MimetypeAudioMpeg,
+	MimetypeAudioMusepack,
+	MimetypeAudioWav,
+	MimetypeAudioXM4A,
+	MimetypeFontWoff,
+	MimetypeFontWoff2,
+	MimetypeImageBmp,
+	MimetypeImageGif,
+	MimetypeImageJpeg,
+	MimetypeImagePng,
+	MimetypeImageSvgXml,
+	MimetypeImageTiff,
+	MimetypeImageWebp,
+	MimetypeImageXIcon,
+	MimetypeModelVndColladaXml,
+	MimetypeModelX3DXml,
+	MimetypeTextHtml,
+	MimetypeTextCss,
+	MimetypeTextPlain,
+	MimetypeTextRtf,
+	MimetypeTextXml,
+	MimetypeTextXLua,
+	MimetypeTextXPerl,
+	MimetypeTextXPhp,
+	MimetypeTextXTcl,
+	MimetypeVideo3GPP,
+	MimetypeVideo3GPP2,
+	MimetypeVideoMp4,
+	MimetypeVideoMpeg,
+	MimetypeVideoQuicktime,
+	MimetypeVideoWebm,
+	MimetypeVideoXFlv,
+	MimetypeVideoXMatroska,
+	MimetypeVideoXMsvideo MimetypeExtension
 )
 
 func init() {
-	MIMETYPE_APPLICATION_DICOM = registerMimeType("application/dicom", "dcm")
-	MIMETYPE_APPLICATION_EPUB_ZIP = registerMimeType("application/epub+zip", "epub")
-	MIMETYPE_APPLICATION_GEO_JSON = registerMimeType("application/geo+json", "geojson")
-	MIMETYPE_APPLICATION_GML_XML = registerMimeType("application/gml+xml", "gml")
-	MIMETYPE_APPLICATION_GPX_XML = registerMimeType("application/gpx+xml", "gpx")
-	MIMETYPE_APPLICATION_GZIP = registerMimeType("application/gzip", "gz")
-	MIMETYPE_APPLICATION_JAR = registerMimeType("application/jar", "jar")
-	MIMETYPE_APPLICATION_JAVASCRIPT = registerMimeType("application/javascript", "js")
-	MIMETYPE_APPLICATION_JSON = registerMimeType("application/json", "json")
-	MIMETYPE_APPLICATION_MSWORD = registerMimeType("application/msword", "doc")
-	MIMETYPE_APPLICATION_OCTET_STREAM = registerMimeType("application/octet-stream", "")
-	MIMETYPE_APPLICATION_OGG = registerMimeType("application/ogg", "ogg")
-	MIMETYPE_APPLICATION_PDF = registerMimeType("application/pdf", "pdf")
-	MIMETYPE_APPLICATION_POSTSCRIPT = registerMimeType("application/postscript", "ps")
-	MIMETYPE_APPLICATION_VND_GARMIN_TCX_XML = registerMimeType("application/vnd.garmin.tcx+xml", "tcx")
-	MIMETYPE_APPLICATION_VND_GOOGLE_EARTH_KML_XML = registerMimeType("application/vnd.google-earth.kml+xml", "kml")
-	MIMETYPE_APPLICATION_VND_MS_EXCEL = registerMimeType("application/vnd.ms-excel", "xls")
-	MIMETYPE_APPLICATION_VND_MS_POWERPOINT = registerMimeType("application/vnd.ms-powerpoint", "ppt")
-	MIMETYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_PRESENTATION = registerMimeType("application/vnd.openxmlformats-officedocument.presentationml.presentation", "pptx")
-	MIMETYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_SHEET = registerMimeType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx")
-	MIMETYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT = registerMimeType("application/vnd.openxmlformats-officedocument.wordprocessingml.document", "docx")
-	MIMETYPE_APPLICATION_WASM = registerMimeType("application/wasm", "wasm")
-	MIMETYPE_APPLICATION_X_7Z_COMPRESSED = registerMimeType("application/x-7z-compressed", "7z")
-	MIMETYPE_APPLICATION_X_CHROME_EXTENSION = registerMimeType("application/x-chrome-extension", "crx")
-	MIMETYPE_APPLICATION_X_JAVA_APPLET = registerMimeType("application/x-java-applet; charset=binary", "class")
-	MIMETYPE_APPLICATION_X_PHOTOSHOP = registerMimeType("application/x-photoshop", "psd")
-	MIMETYPE_APPLICATION_X_PYTHON = registerMimeType("application/x-python", "py")
-	MIMETYPE_APPLICATION_X_SHOCKWAVE_FLASH = registerMimeType("application/x-shockwave-flash", "swf")
-	MIMETYPE_APPLICATION_X_TAR = registerMimeType("application/x-tar", "tar")
-	MIMETYPE_APPLICATION_ZIP = registerMimeType("application/zip", "zip")
-	MIMETYPE_AUDIO_AIFF = registerMimeType("audio/aiff", "aiff")
-	MIMETYPE_AUDIO_AMR = registerMimeType("audio/amr", "amr")
-	MIMETYPE_AUDIO_APE = registerMimeType("audio/ape", "ape")
-	MIMETYPE_AUDIO_BASIC = registerMimeType("audio/basic", "au")
-	MIMETYPE_AUDIO_FLAC = registerMimeType("audio/flac", "flac")
-	MIMETYPE_AUDIO_MIDI = registerMimeType("audio/midi", "midi")
-	MIMETYPE_AUDIO_MP4 = registerMimeType("audio/mp4", "mp4")
-	MIMETYPE_AUDIO_MPEG = registerMimeType("audio/mpeg", "mp3")
-	MIMETYPE_AUDIO_MUSEPACK = registerMimeType("audio/musepack", "mpc")
-	MIMETYPE_AUDIO_WAV = registerMimeType("audio/wav", "wav")
-	MIMETYPE_AUDIO_X_M4A = registerMimeType("audio/x-m4a", "m4a")
-	MIMETYPE_FONT_WOFF = registerMimeType("font/woff", "woff")
-	MIMETYPE_FONT_WOFF2 = registerMimeType("font/woff2", "woff2")
-	MIMETYPE_IMAGE_BMP = registerMimeType("image/bmp", "bmp")
-	MIMETYPE_IMAGE_GIF = registerMimeType("image/gif", "gif")
-	MIMETYPE_IMAGE_JPEG = registerMimeType("image/jpeg", "jpg")
-	MIMETYPE_IMAGE_PNG = registerMimeType("image/png", "png")
-	MIMETYPE_IMAGE_SVG_XML = registerMimeType("image/svg+xml", "svg")
-	MIMETYPE_IMAGE_TIFF = registerMimeType("image/tiff", "tiff")
-	MIMETYPE_IMAGE_WEBP = registerMimeType("image/webp", "webp")
-	MIMETYPE_IMAGE_X_ICON = registerMimeType("image/x-icon", "ico")
-	MIMETYPE_MODEL_VND_COLLADA_XML = registerMimeType("model/vnd.collada+xml", "dae")
-	MIMETYPE_MODEL_X3D_XML = registerMimeType("model/x3d+xml", "x3d")
-	MIMETYPE_TEXT_HTML = registerMimeType("text/html; charset=utf-8", "html")
-	MIMETYPE_TEXT_PLAIN = registerMimeType("text/css", "css")
-	MIMETYPE_TEXT_PLAIN = registerMimeType("text/plain", "txt")
-	MIMETYPE_TEXT_RTF = registerMimeType("text/rtf", "rtf")
-	MIMETYPE_TEXT_XML = registerMimeType("text/xml; charset=utf-8", "xml")
-	MIMETYPE_TEXT_X_LUA = registerMimeType("text/x-lua", "lua")
-	MIMETYPE_TEXT_X_PERL = registerMimeType("text/x-perl", "pl")
-	MIMETYPE_TEXT_X_PHP = registerMimeType("text/x-php; charset=utf-8", "php")
-	MIMETYPE_TEXT_X_TCL = registerMimeType("text/x-tcl", "tcl")
-	MIMETYPE_VIDEO_3GPP = registerMimeType("video/3gpp", "3gp")
-	MIMETYPE_VIDEO_3GPP2 = registerMimeType("video/3gpp2", "3g2")
-	MIMETYPE_VIDEO_MP4 = registerMimeType("video/mp4", "mp4")
-	MIMETYPE_VIDEO_MPEG = registerMimeType("video/mpeg", "mpeg")
-	MIMETYPE_VIDEO_QUICKTIME = registerMimeType("video/quicktime", "mov")
-	MIMETYPE_VIDEO_WEBM = registerMimeType("video/webm", "webm")
-	MIMETYPE_VIDEO_X_FLV = registerMimeType("video/x-flv", "flv")
-	MIMETYPE_VIDEO_X_MATROSKA = registerMimeType("video/x-matroska", "mkv")
-	MIMETYPE_VIDEO_X_MSVIDEO = registerMimeType("video/x-msvideo", "avi")
+	MimetypeApplicationDicom = registerMimeType("application/dicom", "dcm")
+	MimetypeApplicationEpubZip = registerMimeType("application/epub+zip", "epub")
+	MimetypeApplicationGeoJson = registerMimeType("application/geo+json", "geojson")
+	MimetypeApplicationGmlXml = registerMimeType("application/gml+xml", "gml")
+	MimetypeApplicationGpxXml = registerMimeType("application/gpx+xml", "gpx")
+	MimetypeApplicationGzip = registerMimeType("application/gzip", "gz")
+	MimetypeApplicationJar = registerMimeType("application/jar", "jar")
+	MimetypeApplicationJavascript = registerMimeType("application/javascript", "js")
+	MimetypeApplicationJson = registerMimeType("application/json", "json")
+	MimetypeApplicationMsword = registerMimeType("application/msword", "doc")
+	MimetypeApplicationOctetStream = registerMimeType("application/octet-stream", "")
+	MimetypeApplicationOgg = registerMimeType("application/ogg", "ogg")
+	MimetypeApplicationPdf = registerMimeType("application/pdf", "pdf")
+	MimetypeApplicationPostscript = registerMimeType("application/postscript", "ps")
+	MimetypeApplicationVndGarminTcxXml = registerMimeType("application/vnd.garmin.tcx+xml", "tcx")
+	MimetypeApplicationVndGoogleEarthKmlXml = registerMimeType("application/vnd.google-earth.kml+xml", "kml")
+	MimetypeApplicationVndMsExcel = registerMimeType("application/vnd.ms-excel", "xls")
+	MimetypeApplicationVndMsPowerpoint = registerMimeType("application/vnd.ms-powerpoint", "ppt")
+	MimetypeApplicationVndOpenxmlformatsOfficedocumentPresentationmlPresentation = registerMimeType("application/vnd.openxmlformats-officedocument.presentationml.presentation", "pptx")
+	MimetypeApplicationVndOpenxmlformatsOfficedocumentSpreadsheetmlSheet = registerMimeType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx")
+	MimetypeApplicationVndOpenxmlformatsOfficedocumentWordprocessingmlDocument = registerMimeType("application/vnd.openxmlformats-officedocument.wordprocessingml.document", "docx")
+	MimetypeApplicationWasm = registerMimeType("application/wasm", "wasm")
+	MimetypeApplicationX7ZCompressed = registerMimeType("application/x-7z-compressed", "7z")
+	MimetypeApplicationXChromeExtension = registerMimeType("application/x-chrome-extension", "crx")
+	MimetypeApplicationXJavaApplet = registerMimeType("application/x-java-applet; charset=binary", "class")
+	MimetypeApplicationXPhotoshop = registerMimeType("application/x-photoshop", "psd")
+	MimetypeApplicationXPython = registerMimeType("application/x-python", "py")
+	MimetypeApplicationXShockwaveFlash = registerMimeType("application/x-shockwave-flash", "swf")
+	MimetypeApplicationXTar = registerMimeType("application/x-tar", "tar")
+	MimetypeApplicationZip = registerMimeType("application/zip", "zip")
+	MimetypeAudioAiff = registerMimeType("audio/aiff", "aiff")
+	MimetypeAudioAmr = registerMimeType("audio/amr", "amr")
+	MimetypeAudioApe = registerMimeType("audio/ape", "ape")
+	MimetypeAudioBasic = registerMimeType("audio/basic", "au")
+	MimetypeAudioFlac = registerMimeType("audio/flac", "flac")
+	MimetypeAudioMidi = registerMimeType("audio/midi", "midi")
+	MimetypeAudioMp4 = registerMimeType("audio/mp4", "mp4")
+	MimetypeAudioMpeg = registerMimeType("audio/mpeg", "mp3")
+	MimetypeAudioMusepack = registerMimeType("audio/musepack", "mpc")
+	MimetypeAudioWav = registerMimeType("audio/wav", "wav")
+	MimetypeAudioXM4A = registerMimeType("audio/x-m4a", "m4a")
+	MimetypeFontWoff = registerMimeType("font/woff", "woff")
+	MimetypeFontWoff2 = registerMimeType("font/woff2", "woff2")
+	MimetypeImageBmp = registerMimeType("image/bmp", "bmp")
+	MimetypeImageGif = registerMimeType("image/gif", "gif")
+	MimetypeImageJpeg = registerMimeType("image/jpeg", "jpg")
+	MimetypeImagePng = registerMimeType("image/png", "png")
+	MimetypeImageSvgXml = registerMimeType("image/svg+xml", "svg")
+	MimetypeImageTiff = registerMimeType("image/tiff", "tiff")
+	MimetypeImageWebp = registerMimeType("image/webp", "webp")
+	MimetypeImageXIcon = registerMimeType("image/x-icon", "ico")
+	MimetypeModelVndColladaXml = registerMimeType("model/vnd.collada+xml", "dae")
+	MimetypeModelX3DXml = registerMimeType("model/x3d+xml", "x3d")
+	MimetypeTextHtml = registerMimeType("text/html; charset=utf-8", "html")
+	MimetypeTextPlain = registerMimeType("text/css", "css")
+	MimetypeTextPlain = registerMimeType("text/plain", "txt")
+	MimetypeTextRtf = registerMimeType("text/rtf", "rtf")
+	MimetypeTextXml = registerMimeType("text/xml; charset=utf-8", "xml")
+	MimetypeTextXLua = registerMimeType("text/x-lua", "lua")
+	MimetypeTextXPerl = registerMimeType("text/x-perl", "pl")
+	MimetypeTextXPhp = registerMimeType("text/x-php; charset=utf-8", "php")
+	MimetypeTextXTcl = registerMimeType("text/x-tcl", "tcl")
+	MimetypeVideo3GPP = registerMimeType("video/3gpp", "3gp")
+	MimetypeVideo3GPP2 = registerMimeType("video/3gpp2", "3g2")
+	MimetypeVideoMp4 = registerMimeType("video/mp4", "mp4")
+	MimetypeVideoMpeg = registerMimeType("video/mpeg", "mpeg")
+	MimetypeVideoQuicktime = registerMimeType("video/quicktime", "mov")
+	MimetypeVideoWebm = registerMimeType("video/webm", "webm")
+	MimetypeVideoXFlv = registerMimeType("video/x-flv", "flv")
+	MimetypeVideoXMatroska = registerMimeType("video/x-matroska", "mkv")
+	MimetypeVideoXMsvideo = registerMimeType("video/x-msvideo", "avi")
 }
 
-func registerMimeType(mimeType, ext string) MIMETYPE_EXTENSION {
-	mt := MIMETYPE_EXTENSION{
+func registerMimeType(mimeType, ext string) MimetypeExtension {
+	mt := MimetypeExtension{
 		MimeType: mimeType,
 		Ext:      ext,
 	}
 
-	MIMETYPES = append(MIMETYPES, mt)
+	Mimetypes = append(Mimetypes, mt)
 
 	return mt
 }
 
-func DetectMimeType(filename string, buf []byte) MIMETYPE_EXTENSION {
+func DetectMimeType(filename string, buf []byte) MimetypeExtension {
 	if filename != "" {
 		ext := filepath.Ext(filename)
 		if ext != "" {
@@ -187,7 +187,7 @@ func DetectMimeType(filename string, buf []byte) MIMETYPE_EXTENSION {
 
 			ext = strings.ToLower(ext)
 
-			for _, mt := range MIMETYPES {
+			for _, mt := range Mimetypes {
 				if mt.Ext == ext {
 					return mt
 				}
@@ -197,20 +197,20 @@ func DetectMimeType(filename string, buf []byte) MIMETYPE_EXTENSION {
 
 	t, err := filetype.Match(buf)
 	if t.MIME.Value != "" && err == nil {
-		return MIMETYPE_EXTENSION{t.MIME.Value, t.Extension}
+		return MimetypeExtension{t.MIME.Value, t.Extension}
 	}
 
 	mime, ext := mimetype.Detect(buf)
 
 	if len(mime) > 0 {
-		return MIMETYPE_EXTENSION{mime, ext}
+		return MimetypeExtension{mime, ext}
 	}
 
-	return MIMETYPE_APPLICATION_OCTET_STREAM
+	return MimetypeApplicationOctetStream
 }
 
 func ReadHeader(path string) ([]byte, error) {
-	byteSlice := make([]byte, MIMETYPE_HEADER_LEN)
+	byteSlice := make([]byte, MimetypeHeaderLen)
 
 	file, err := os.Open(path)
 	if err != nil {
