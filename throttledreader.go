84bd1ae5eb06d84dc36cfb888139dc045e2748a4
mpetavy <mpetavy>
2020-01-16 15:17:14 +0100

    throttledreader/throttledwriter corrected,throttledreaer reactivated, netio throttledreader/throttledwriter reactivated

--- throttledreader.go
+++ throttledreader.go
@@ -8,26 +8,59 @@ import (
 type throttledReader struct {
 	reader       io.Reader
 	bytesPerSecs int
+	lastSec      time.Time
+	lastSecRead  int
 }
 
-func (this *throttledReader) Read(p []byte) (int, error) {
-	start := time.Now()
-
-	n, err := this.reader.Read(p)
-
-	if this.bytesPerSecs > 0 {
-		mustMilli := int(float64(n) / float64(this.bytesPerSecs) * 1000)
-
-		target := start.Add(time.Millisecond * time.Duration(mustMilli))
-
-		//d := target.Sub(time.Now())
-		d := target.Sub(start)
-		if d > 0 {
-			time.Sleep(d)
-		}
+func (this *throttledReader) Read(p []byte) (n int, err error) {
+	if this.bytesPerSecs == 0 {
+		return this.reader.Read(p)
 	}
 
-	return n, err
+	lenP := len(p)
+	index := 0
+
+	curSec := time.Now().Truncate(time.Second)
+	if this.lastSec.IsZero() || this.lastSec.Before(curSec) {
+		this.lastSec = curSec
+		this.lastSecRead = 0
+	}
+
+	for {
+		sleep := false
+
+		remainAllowedLen := this.bytesPerSecs - this.lastSecRead
+		remainBufferLen := lenP - index
+
+		if remainBufferLen > remainAllowedLen {
+			remainBufferLen = remainAllowedLen
+			sleep = true
+		}
+
+		n, err := this.reader.Read(p[index : index+remainBufferLen])
+
+		index += n
+		this.lastSecRead += n
+
+		if err != nil {
+			return index, err
+		}
+
+		if index == lenP {
+			return index, nil
+		}
+
+		if sleep {
+			this.lastSec = this.lastSec.Add(time.Second)
+			this.lastSecRead = 0
+
+			d := this.lastSec.Sub(time.Now())
+
+			if d > 0 {
+				time.Sleep(d)
+			}
+		}
+	}
 }
 
 func NewThrottledReader(reader io.Reader, bytesPerSecs int) io.Reader {
