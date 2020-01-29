2ec33d685860a107379e992980909e63b3ad6e01
mpetavy <mpetavy>
2020-01-29 11:07:27 +0100

    Expert view added

---
+++ echo.go
@@ -0,0 +1,34 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type echoLogger struct{}
+
+func (this echoLogger) Write(p []byte) (int, error) {
+	msg := string(p)
+	isError := false
+
+	m := make(map[string]interface{})
+
+	err := json.Unmarshal(p, &m)
+	if err == nil {
+		v, ok := m["error"]
+
+		isError = ok && fmt.Sprintf("%v", v) != ""
+	}
+
+	if isError {
+		Error(fmt.Errorf("Echo: %s", msg))
+	} else {
+		Debug("Echo: %s", msg)
+	}
+
+	return len(p), nil
+}
+
+func NewEchoLogger() echoLogger {
+	return echoLogger{}
+}
