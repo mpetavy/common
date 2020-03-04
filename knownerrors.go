ef864bc8670e9ab51c93602de42a6370d3cffa81
mpetavy <mpetavy>
2020-03-04 17:57:52 +0100

    ParseJsonDateTime, centralized suppressed errors, goreleaser with LDFLAGs for Date, Git tag

--- knownerrors.go
+++ knownerrors.go
@@ -1,6 +1,16 @@
 package common
 
-import "strings"
+import (
+	"strings"
+)
+
+var (
+	suppressError = []string{
+		"http2",
+		"wsasend",
+		"remote error: tls: unknown certificate",
+	}
+)
 
 func IsErrNetClosing(err error) bool {
 	return strings.Index(strings.ToLower(err.Error()), "use of closed network connection") != -1
@@ -8,4 +18,12 @@ func IsErrNetClosing(err error) bool {
 
 func IsErrUnexpectedEOF(err error) bool {
 	return strings.Index(strings.ToLower(err.Error()), "unexpected eof") != -1
+}
+
+func IsSuppressedError(err error) bool {
+	return IndexOf(suppressError, err.Error()) != -1
+}
+
+func IsSuppressedErrorMessage(err string) bool {
+	return IndexOf(suppressError, err) != -1
 }
