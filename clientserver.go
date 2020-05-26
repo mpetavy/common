64449e1c9bc9a7de0b9801ddd3989517ee736f97
mpetavy <mpetavy>
2020-05-26 15:48:13 +0200

    flag callback request file flag callback response file TryEdgeboxCallback added

---
+++ clientserver.go
@@ -0,0 +1,172 @@
+package common
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"golang.org/x/crypto/sha3"
+	"hash"
+	"io"
+	"net"
+)
+
+type TCPServer struct {
+	useTls       bool
+	useTlsVerify bool
+	address      string
+	ctx          context.Context
+	cancel       context.CancelFunc
+	Hash         hash.Hash
+	Conn         net.Conn
+	listener     net.Listener
+}
+
+func NewTCPServer(useTls bool, useTlsVerify bool, address string) *TCPServer {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &TCPServer{
+		useTls:       useTls,
+		useTlsVerify: useTlsVerify,
+		address:      address,
+		ctx:          ctx,
+		cancel:       cancel,
+		Hash:         sha3.New512(),
+	}
+}
+
+func (this *TCPServer) Serve() ([]byte, error) {
+	if this.useTls {
+		tlsPackage, err := GetTlsPackage()
+		if Error(err) {
+			return nil, err
+		}
+
+		if this.useTlsVerify {
+			tlsPackage.Config.ClientAuth = tls.RequireAndVerifyClientCert
+		}
+
+		this.listener, err = tls.Listen("tcp", this.address, &tlsPackage.Config)
+		if Error(err) {
+			return nil, err
+		}
+	} else {
+		tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%s", this.address))
+		if Error(err) {
+			return nil, err
+		}
+
+		this.listener, err = net.ListenTCP("tcp", tcpAddr)
+		if Error(err) {
+			return nil, err
+		}
+	}
+
+	var err error
+
+	if this.useTls {
+		this.Conn, err = this.listener.Accept()
+		Error(err)
+
+		tcpConn, ok := this.Conn.(*net.TCPConn)
+		if ok {
+			Error(tcpConn.SetLinger(0))
+		}
+
+		tlsConn, ok := this.Conn.(*tls.Conn)
+		if ok {
+			err := tlsConn.Handshake()
+
+			if Error(err) {
+				Error(this.Conn.Close())
+			}
+		}
+	} else {
+		this.Conn, err = this.listener.Accept()
+		Error(err)
+	}
+
+	tcpConn, ok := this.Conn.(*net.TCPConn)
+	if ok {
+		Error(tcpConn.SetLinger(0))
+	}
+
+	defer func() {
+		Error(this.Conn.Close())
+		if this.listener != nil {
+			Error(this.listener.Close())
+		}
+	}()
+
+	this.Hash.Reset()
+
+	_, err = io.Copy(this.Hash, this.Conn)
+
+	return this.Hash.Sum(nil), err
+}
+
+type TCPClient struct {
+	useTls       bool
+	useTlsVerify bool
+	address      string
+	ctx          context.Context
+	cancel       context.CancelFunc
+	Conn         net.Conn
+}
+
+func NewTCPClient(useTls bool, useTlsVerify bool, address string) *TCPClient {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &TCPClient{
+		useTls:       useTls,
+		useTlsVerify: useTlsVerify,
+		address:      address,
+		ctx:          ctx,
+		cancel:       cancel,
+	}
+}
+
+func (this *TCPClient) Connect() (io.ReadWriter, error) {
+	if this.useTls {
+		tlsPackage, err := GetTlsPackage()
+		if Error(err) {
+			return nil, err
+		}
+
+		hostname, _, err := net.SplitHostPort(this.address)
+		if Error(err) {
+			return nil, err
+		}
+
+		if hostname == "" {
+			hostname = "localhost"
+		}
+
+		// set hostname for self-signed cetificates
+		tlsPackage.Config.ServerName = hostname
+		tlsPackage.Config.InsecureSkipVerify = !this.useTlsVerify
+
+		this.Conn, err = tls.Dial("tcp", this.address, &tlsPackage.Config)
+		if Error(err) {
+			return nil, err
+		}
+
+		tlsSocket, ok := this.Conn.(*tls.Conn)
+		if ok {
+			if !tlsSocket.ConnectionState().HandshakeComplete {
+				return nil, fmt.Errorf("TLS handshake not completed")
+			}
+		}
+	} else {
+		tcpAddr, err := net.ResolveTCPAddr("tcp", this.address)
+		if Error(err) {
+			return nil, err
+		}
+
+		this.Conn, err = net.DialTCP("tcp", nil, tcpAddr)
+		if Error(err) {
+			return nil, err
+		}
+	}
+
+	return this.Conn, nil
+}
