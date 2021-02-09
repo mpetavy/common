44443422dab07fe87f45454263927c44870ce77f
mpetavy <mpetavy>
2021-02-09 18:27:46 +0100

    refactoring

--- io.go
+++ io.go
@@ -76,7 +76,7 @@ func init() {
 		Error(deleteTempDir())
 	})
 
-	FlagCountBackups = flag.Int("bak", 3, "amount of file backups")
+	FlagCountBackups = flag.Int("io.filebackups", 3, "amount of file backups")
 }
 
 // AppCleanup cleans up all remaining objects
