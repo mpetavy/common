a8a6b005509edb5a7f1a9414c2318903cf749be1
mpetavy <mpetavy>
2021-06-24 10:15:17 +0200

    logging with US date format

--- logging.go
+++ logging.go
@@ -486,7 +486,7 @@ func appendLog(level int, ri RuntimeInfo, msg string, err error) {
 	entry := logEntry{
 		levelInt: level,
 		Level:    strings.ToUpper(levelToString(level)),
-		Clock:    time.Now().Format(DateTimeMilliMask),
+		Clock:    time.Now().Format(SortedDateTimeMilliMask),
 		Runtime:  ri.String(),
 		Msg:      Capitalize(strings.TrimRight(strings.TrimSpace(msg), "\r\n")),
 	}
