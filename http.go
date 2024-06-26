8372cebb6b58699b179774c10c2d8e74798af01f
mpetavy <mpetavy>
2024-06-26 18:17:37 +0200

    RestURL refactor

--- http.go
+++ http.go
@@ -385,13 +385,28 @@ func ReadBodyJSON[T any](r io.ReadCloser) ([]T, bool, error) {
 	return records, isArray, nil
 }
 
+type RestURLField struct {
+	Name        string
+	Description string
+	Default     string
+	Optional    bool
+}
+
 type RestURL struct {
-	Method   string
-	Resource string
+	Description string
+	Method      string
+	Resource    string
+	Consumes    []string
+	Produces    []string
+	Success     []int
+	failure     []int
+	Headers     []RestURLField
+	PathValues  []RestURLField
+	PathParams  []RestURLField
 }
 
 func NewRestURL(method string, resource string) *RestURL {
-	return &RestURL{method, resource}
+	return &RestURL{Method: method, Resource: resource}
 }
 
 func (u *RestURL) String() string {
@@ -413,4 +428,55 @@ func (u *RestURL) Format(args ...any) string {
 
 func (u *RestURL) URL(address string, args ...any) string {
 	return fmt.Sprintf("%s%s", address, u.Format(args...))
+}
+
+func (u *RestURL) Validate(r *http.Request) error {
+	if u.Method != r.Method {
+		return fmt.Errorf("invalid HTTP method")
+	}
+
+	rPaths := Split(r.URL.Path, "/")
+	uPaths := Split(u.Resource, "/")
+
+	if !strings.Contains(u.Resource, "*") {
+		if len(rPaths) != len(uPaths) {
+			return fmt.Errorf("invalid amount of HTTP request path")
+		}
+	}
+
+	for _, param := range u.PathParams {
+		if !r.URL.Query().Has(param.Name) {
+			if param.Optional {
+				if param.Default != "" {
+					r.URL.Query().Add(param.Name, param.Default)
+				}
+			} else {
+				return fmt.Errorf("missing HTTP header: %s", param.Name)
+			}
+		}
+	}
+
+	for _, header := range u.Headers {
+		if r.Header.Get(header.Name) == "" {
+			if header.Optional {
+				if header.Default != "" {
+					r.Header.Add(header.Name, header.Default)
+				}
+			} else {
+				return fmt.Errorf("missing HTTP header: %s", header.Name)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (u *RestURL) SwaggerInfo() string {
+	sb := strings.Builder{}
+
+	sb.WriteString(fmt.Sprintf("@Title %s\n", u.Resource))
+	sb.WriteString(fmt.Sprintf("@Description %s\n", u.Description))
+	sb.WriteString(fmt.Sprintf("@Accept %s\n", strings.Join(u.Consumes, " ")))
+
+	return sb.String()
 }
